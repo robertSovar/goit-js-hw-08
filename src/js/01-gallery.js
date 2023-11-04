61953614f352d428e8f89a8e1cb36bc50b8839a0
robertSovar <133514926+robertSovar@users.noreply.github.com>
2023-11-04 21:31:09 +0200

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from "./gallery-items.js";
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryItemsElement = document.querySelector(".gallery");
+const photoMarkup = createGalleryItems(galleryItems);
+
+function createGalleryItems(element) {
+    return element.map((preview, original, description) => {
+        return `
+            <a href="${original}" class="gallery__item">
+                <img src="${preview}" alt="${description}" class="gallery__image">
+            </a>`
+    }).join('');
+}
+
+galleryItemsElement.insertAdjacentElement("beforeend", photoMarkup);
+
+const galleryHandler = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay : 250
+});
+
+galleryHandler.on("show.simplelightbox");
