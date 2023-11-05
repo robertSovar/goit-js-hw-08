1a17fa4de941cd5ea1ccbb2d45c148a9a06b98d7
robertSovar <133514926+robertSovar@users.noreply.github.com>
2023-11-05 10:07:23 +0200

    finished second exercise

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onTimeUpdate = throttle((data) => {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}, 1000);
+
+player.on('timeupdate', onTimeUpdate);
+
+const savedTime = localStorage.getItem('videoplayer-current-time' || 0);
+
+player.setCurrentTime(savedTime).catch((error) => {
+ if (error.name === 'RangeError') {
+    return;
+ } return;
+});
